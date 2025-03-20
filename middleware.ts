9bfe54bba2d6184cea61b30a3299a48bc2298ddd
Brooks Garrett <brooks@brooksgarrett.com>
2025-03-19 18:05:44 -0700

    Add Google auth

--- middleware.ts
+++ middleware.ts
@@ -1,9 +1,8 @@
-import NextAuth from 'next-auth';
-import { authConfig } from './auth.config';
+import { auth } from "@/auth"
  
-export default NextAuth(authConfig).auth;
- 
-export const config = {
-  // https://nextjs.org/docs/app/building-your-application/routing/middleware#matcher
-  matcher: ['/((?!api|_next/static|_next/image|.*\\.png$).*)'],
-};
+export default auth((req) => {
+  if (!req.auth && req.nextUrl.pathname.startsWith("/dashboard")) {
+    const newUrl = new URL("/", req.nextUrl.origin)
+    return Response.redirect(newUrl)
+  }
+})
